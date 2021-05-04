6f9150ac1ac383a0a0be5536f25cfb67d5a36430
JonahValdez <valdezjonah111@gmail.com>
2021-05-04 16:05:28 -0500

    Added Boxes to Interact
    Added boxes that are interactable. Added UI that shows collected pieces

---
+++ Assets/Scripts/BoxInteraction.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoxInteraction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isEPressed = false;
+    [SerializeField] private Text pieceText;
+    public string pickedUp;
+    public bool peiceOne = false;
+    private bool canInteract = false;
+    [SerializeField] private GameObject interactPanel;
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        pieceText.text = "Press E to Interact";
+        interactPanel.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) == true && canInteract == true)
+        {
+            
+            isEPressed = true;
+            updateUI();
+            
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        pieceText.text = "Press E to Interact";
+        interactPanel.SetActive(true);
+        canInteract = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (isEPressed == true)
+        {
+            pieceText.text = "";
+            //peiceOne = true;
+            other.isTrigger = false;
+        }
+        canInteract = false;
+        interactPanel.SetActive(false);
+        
+    }
+    private void updateUI()
+    {
+        pieceText.text = "You found " + pickedUp;
+        
+    }
+    public bool wasEPressed()
+    {
+        return isEPressed;
+    }
+
+}
